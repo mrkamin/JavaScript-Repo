5d613d0ef44a6bc872c4c93f2675188f56154cce
Rafi <mrkamin2@gmail.com>
2024-09-06 18:38:10 +0500

    Change the textContent of html element with Js file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-console.log(`Hello`);
-console.log(`I Like Pizza!`);
-window.alert(`This is an alert!`);
-window.alert(`I like pizza!`);
+// console.log(`Hello`);
+// console.log(`I Like Pizza!`);
+// window.alert(`This is an alert!`);
+// window.alert(`I like pizza!`);
+
+document.getElementById("myH1").textContent = `Hello`,
+document.getElementById("myP").textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit cumque numquam aspernatur qui laboriosam! Aperiam inventore, labore cupiditate aliquam minima nam quidem asperiores nihil, dignissimos architecto dicta necessitatibus corporis ratione.`
