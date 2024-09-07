b07a9236316e724673d1c9df87d00f1dcc3f7e59
Rafi <mrkamin2@gmail.com>
2024-09-07 12:14:49 +0500

    Variable and data type example

--- index.js
+++ index.js
@@ -3,5 +3,20 @@
 // window.alert(`This is an alert!`);
 // window.alert(`I like pizza!`);
 
-document.getElementById("myH1").textContent = `Hello`,
-document.getElementById("myP").textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit cumque numquam aspernatur qui laboriosam! Aperiam inventore, labore cupiditate aliquam minima nam quidem asperiores nihil, dignissimos architecto dicta necessitatibus corporis ratione.`
+// document.getElementById("myH1").textContent = `Hello`,
+// document.getElementById("myP").textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit cumque numquam aspernatur qui laboriosam! Aperiam inventore, labore cupiditate aliquam minima nam quidem asperiores nihil, dignissimos architecto dicta necessitatibus corporis ratione.`;
+
+// let x;
+// x = 100;
+//  console.log(x);
+
+ let x = 100;
+ console.log(x);
+
+ let age = 25;
+ let price = 10.99;
+ let gpa = 2.1;
+
+ console.log(`Yor are ${age} years old`);
+ console.log(`The price is $${price}`);
+ console.log(`My gpa is ${gpa}`)
