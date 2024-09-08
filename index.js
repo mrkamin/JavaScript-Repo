fb0c4d8bd8907077f0902f18ef5fdb9aae539d4a
Rafi <mrkamin2@gmail.com>
2024-09-08 17:22:53 +0500

    Aratematic operator example

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 let students = 30;
 
-students = students + 1;
+// students = students + 1;
+// console.log(students); //31
+// students = students - 1;
+// console.log(students); //30
+// students = students * 2;
+// console.log(students);//60
+// students = students / 2;
+// console.log(students); //30
+// students = students ** 2;
+// console.log(students); //900
+
+// let extraStudents = students % 3;
+
+// console.log(extraStudents) //0
+
+students = students += 1;
 console.log(students); //31
-students = students - 1;
+students = students -= 1;
 console.log(students); //30
-students = students * 2;
+students = students *= 2;
 console.log(students);//60
-students = students / 2;
+students = students /= 2;
 console.log(students); //30
-students = students ** 2;
+students = students **= 2;
 console.log(students); //900
 
-let extraStudents = students % 3;
+let extraStudents1 = students %= 3;
 
-console.log(extraStudents) //0
+console.log(extraStudents1) //0
 
