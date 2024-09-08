3e29699d836dff97c87c81a1b0b2f06c1d797ec8
Rafi <mrkamin2@gmail.com>
2024-09-08 17:02:36 +0500

    Add textcontent to the html element through js code

--- index.js
+++ index.js
@@ -1,35 +1,7 @@
-// console.log(`Hello`);
-// console.log(`I Like Pizza!`);
-// window.alert(`This is an alert!`);
-// window.alert(`I like pizza!`);
+let fullName = "Rafi Amin";
+let age = 25;
+let isStudent = false;
 
-// document.getElementById("myH1").textContent = `Hello`,
-// document.getElementById("myP").textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit cumque numquam aspernatur qui laboriosam! Aperiam inventore, labore cupiditate aliquam minima nam quidem asperiores nihil, dignissimos architecto dicta necessitatibus corporis ratione.`;
-
-// let x;
-// x = 100;
-//  console.log(x);
-
-//  let x = 100;
-//  console.log(x);
-
-//  let age = 25;
-//  let price = 10.99;
-//  let gpa = 2.1;
-
-//  conole.log(typeof age);
-//  console.log(`Yor are ${age} years old`);
-//  console.log(`The price is $${price}`);
-//  console.log(`My gpa is ${gpa}`)
-
-// let firstName = "Rafi";
-
-// console.log(typeof firstName);
-// console.log(firstName);
-
-let online = true;
-let forSale = false;
-
-console.log(`I am online: ${online}`);
-console.log(`These are for sale ${forSale}`);
-
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrollec: ${isStudent}`;
