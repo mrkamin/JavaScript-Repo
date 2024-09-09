89c022850280cc10c6cd2f67698373d922015f60
Rafi <mrkamin2@gmail.com>
2024-09-09 14:17:28 +0500

    Accept the user input easy way example

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-let result = 12 % 5 + 8 /2 ;
-
-console.log(result) //6
-
-let result2 = 6/2 ** (2+5);
-console.log(result2) //0.046875
+let userName = window.prompt("What's your username?");
+console.log(userName)
