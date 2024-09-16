6069274e90ff708ffa49d31d5b0503276150efec
Rafi <mrkamin2@gmail.com>
2024-09-16 16:23:09 +0500

    Function Scope example

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 // Global Scope 
 // Variables are accessible everywhere in the code
 
-const globalScopeExample = "I am global Scope Example";
+// const globalScopeExample = "I am global Scope Example";
 
-const globalScopeExample2 = true; 
+// const globalScopeExample2 = true; 
 
-console.log("global Scope test", globalScopeExample);
+// console.log("global Scope test", globalScopeExample);
 
-function globalScopeTest(){
-    console.log("global Scope test2", globalScopeExample)
-    if(globalScopeExample2 === true){
-        console.log("global Scope test3", globalScopeExample)
-    }
-}
+// function globalScopeTest(){
+//     console.log("global Scope test2", globalScopeExample)
+//     if(globalScopeExample2 === true){
+//         console.log("global Scope test3", globalScopeExample)
+//     }
+// }
 
-globalScopeTest()
+// globalScopeTest()
 
 // Function Scope
 
+function functionScopeTest(){
+   const functionScopeTestVarible = "I am Function scope";
+    console.log("function scope test inside function", functionScopeTestVarible);
+}
+functionScopeTest()
+
+console.log("function scope test outside function", functionScopeTestVarible);
 // Block Scope
 
 // Module Scope
