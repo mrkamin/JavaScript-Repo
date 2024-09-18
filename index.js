c8c62017f9aaed77a2f719c25b44bf48a3e10518
Rafi <mrkamin2@gmail.com>
2024-09-18 13:52:26 +0500

    Block scop example

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-// Global Scope 
-// Variables are accessible everywhere in the code
+const blockScopeTest = true;
 
-// const globalScopeExample = "I am global Scope Example";
-
-// const globalScopeExample2 = true; 
-
-// console.log("global Scope test", globalScopeExample);
-
-// function globalScopeTest(){
-//     console.log("global Scope test2", globalScopeExample)
-//     if(globalScopeExample2 === true){
-//         console.log("global Scope test3", globalScopeExample)
-//     }
-// }
-
-// globalScopeTest()
-
-// Function Scope
-
-function functionScopeTest(){
-   const functionScopeTestVarible = "I am Function scope";
-    console.log("function scope test inside function", functionScopeTestVarible);
-}
-functionScopeTest()
-
-console.log("function scope test outside function", functionScopeTestVarible);
-// Block Scope
-
-// Module Scope
+if(blockScopeTest === true){
+    console.log("I am block scope test");
+};
