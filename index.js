e20aa334695d18915b62277d4f06b6bf2e900761
Rafi <mrkamin2@gmail.com>
2024-10-09 08:17:20 +0500

    Change the string type to number and boolean

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const blockScopeTest = true;
+let x = "Pizza";
+let y = "Pizza";
+let z = "Pizza";
 
-if(blockScopeTest === true){
-    let blockScopeTestInsideBlok = "I am block scope";
-    console.log(blockScopeTestInsideBlok)
-};
- 
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z)
