687ee014153bf58f62aba7064ed99ce8538fc8a9
Rafi <mrkamin2@gmail.com>
2024-10-12 07:49:20 +0500

    Apply a fun to decrease the counter

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const PI = 3.14159;
-let radius;
-let circumference;
+let count = 0;
 
-
-document.getElementById("myBtn").onclick = () => {
-    document.getElementById('myInput').ariaValueMax;
-    radius = Number(radius);
-    circumference = 2* PI* radius;
-    document.getElementById('myH3').textContent = circumference + 'cm'
+document.getElementById('decrease').onclick = () => {
+    count--
+    console.log(count)
+    document.getElementById("mylabel").textContent = count
 }
+
+
+
