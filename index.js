ddd6a064cb31514e5aba5f143e668b86f31531b3
Rafi <mrkamin2@gmail.com>
2024-10-12 07:51:11 +0500

    Make the code dry and clean

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ let count = 0;
 
 document.getElementById('decrease').onclick = () => {
     count--
-    console.log(count)
     document.getElementById("mylabel").textContent = count
 }
 
 document.getElementById('increase').onclick = () => {
     count++
-    console.log(count)
     document.getElementById("mylabel").textContent = count
 }
 
 document.getElementById('reset').onclick = () => {
     count = 0
-    console.log(count)
     document.getElementById("mylabel").textContent = count
 }
