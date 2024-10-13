5524cdbca7b677e705e0fd9df9ad2d3b55a0aa00
Rafi <mrkamin2@gmail.com>
2024-10-13 09:32:14 +0500

    Math.trunc method exmple

--- math.js
+++ math.js
@@ -24,6 +24,14 @@ let k = 3.4;
 l = Math.ceil(k);
 console.log(l);// 4
 
+let m = 3.99;
+n = Math.trunc(m);
+console.log("N =",n);// 3
+
+let o = 3.3;
+p = Math.trunc(o);
+console.log("P =",p);//3
+
 let x = 3;
 let y = 2;
 let z = 1;
