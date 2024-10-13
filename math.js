9378e42e7867129cd44609b19b88973c0561684f
Rafi <mrkamin2@gmail.com>
2024-10-13 08:57:27 +0500

    Math.round method example

---
+++ math.js
@@ -0,0 +1,17 @@
+// Math = built-in object that provides a collection of properties and methods
+let a = 3.5
+
+b = Math.round(a);
+console.log(b) // out = 4
+
+let c = 3.4
+d = Math.round(c);
+console.log(d);//3
+
+
+
+
+let x = 3;
+let y = 2;
+let z = 1;
+
