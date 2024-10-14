4c4c06e169dffa650141853f48ae742bd99556ec
Rafi <mrkamin2@gmail.com>
2024-10-14 08:26:00 +0500

    Math.sqrt method example

--- math.js
+++ math.js
@@ -1,40 +1,6 @@
-// Math = built-in object that provides a collection of properties and methods
-let a = 3.5
-
-b = Math.round(a);
-console.log(b) // out = 4
-
-let c = 3.4
-d = Math.round(c);
-console.log(d);// out = 3
-
-let e = 3.5;
-f = Math.floor(e);
-console.log(f); // out = 3
-
-let g = 3.99;
-h = Math.floor(e);
-console.log(h); // out = 3
-
-let i = 3.99;
-j = Math.ceil(i);
-console.log(j); // 4
-
-let k = 3.4;
-l = Math.ceil(k);
-console.log(l);// 4
-
-let m = 3.99;
-n = Math.trunc(m);
-console.log("N =",n);// 3
-
-let o = 3.3;
-p = Math.trunc(o);
-console.log("P =",p);//3
-
-let x = 3;
+let x = 81;
 let y = 2;
-let z = 9;
+let z;
 
-q = Math.pow(x,y);
-console.log("Q =",q) // out = 9
+z = Math.sqrt(x);
+console.log('Z =',z);
