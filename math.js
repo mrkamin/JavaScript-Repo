17203df9ee7d098442e6a8c525b638fdbd4a1b75
Rafi <mrkamin2@gmail.com>
2024-10-14 08:43:45 +0500

    Math.max method example

--- math.js
+++ math.js
@@ -1,15 +1,16 @@
 let x = 5;
 let y = 2;
-let z;
+let z = 9;
 
 // z = Math.sqrt(x);
 // z = Math.log(x);
 // z = Math.cos(x);
 //z = Math.tan(x);
 //z = Math.abs(x);
-z = Math.sign(x)
+//z = Math.sign(x);
+z = Math.max(x,y,z)
 
 
 
-console.log('Z =',z);//1
+console.log('Z =',z);//9
 
