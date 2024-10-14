d49f56efa85841d4ef8655c8d0f0fe8b4b0d769e
Rafi <mrkamin2@gmail.com>
2024-10-14 08:40:17 +0500

    Math.abs method example

--- math.js
+++ math.js
@@ -1,12 +1,14 @@
-let x = 1;
+let x = -5;
 let y = 2;
 let z;
 
 // z = Math.sqrt(x);
 // z = Math.log(x);
 // z = Math.cos(x);
-z = Math.tan(x);
+//z = Math.tan(x);
+z = Math.abs(x);
 
 
-console.log('Z =',z);//1.5
+
+console.log('Z =',z);//5
 
