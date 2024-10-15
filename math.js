1fe0d3f3eae5fe6e5e43b7cc09347771a81be46c
Rafi <mrkamin2@gmail.com>
2024-10-15 15:01:23 +0500

    Math.random method example

--- math.js
+++ math.js
@@ -1,17 +1,5 @@
-let x = 5;
-let y = 2;
-let z = 9;
+// random = number generator
 
-// z = Math.sqrt(x);
-// z = Math.log(x);
-// z = Math.cos(x);
-//z = Math.tan(x);
-//z = Math.abs(x);
-//z = Math.sign(x);
-//z = Math.max(x,y,z)
-z = Math.min(x,y,z);
+let random = Math.random()*6
 
-
-
-console.log('Z =',z);//2
-
+console.log(random);
