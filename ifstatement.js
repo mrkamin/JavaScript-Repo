8b5d64627f1fa1fd1b61f38fdd0f732af9bb03d8
Rafi <mrkamin2@gmail.com>
2024-10-21 09:09:17 +0500

    If statement first example

---
+++ ifstatement.js
@@ -0,0 +1,9 @@
+// IF STATEMENT = if a condition is true, execute some code,
+//                if not, do something else
+
+let age = 25;
+if(age >= 19){
+    console.log('Your old enogh to enter this site')
+}else{
+    "You must be 18+ to enter this site"
+}
