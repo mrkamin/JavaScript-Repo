27af48f3a3bcd557472ebcd8860354b0ff4d8b61
Rafi <mrkamin2@gmail.com>
2024-10-21 08:56:41 +0500

    Clean the code

--- math.js
+++ math.js
@@ -1,13 +1,11 @@
 // random = number generator
-const randoLabel = document.getElementById('random-label');
+const randomLabel = document.getElementById('random-label');
 const randdomBtn = document.getElementById('random-btn');
 const min = 1;
 const max = 100;
 let randomNum = '';
 
 randdomBtn.onclick = () => {
-    const myrandom = Math.floor(Math.random(min)*max)
-    console.log(myrandom)
-    randomNum = myrandom
-    randoLabel.textContent = randomNum
+    randomNum = Math.floor(Math.random(min)*max)
+    randomLabel.textContent = randomNum
 }
