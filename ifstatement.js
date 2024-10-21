c195a5a8fe7f5ad26042a35db8d9525201d9c9c4
Rafi <mrkamin2@gmail.com>
2024-10-21 09:15:43 +0500

    If statement with boolean example

--- ifstatement.js
+++ ifstatement.js
@@ -1,9 +1,18 @@
 // IF STATEMENT = if a condition is true, execute some code,
 //                if not, do something else
 
-let age = 25;
+const age = 25;
 if(age >= 19){
     console.log('Your old enogh to enter this site')
 }else{
     "You must be 18+ to enter this site"
 }
+
+let isStudent = false;
+
+if(isStudent){
+    console.log("You are a student")
+}else{
+    console.log("You are not a student")
+}
+
